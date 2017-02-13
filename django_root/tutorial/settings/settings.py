e2c456bcca2a6aeb1310538fdfdfb89eb752ae87
Aleksander Babko <shb@apriorit.com>
2017-02-13 10:21:16 +0200

    docker-compose

--- django_root/tutorial/settings/settings.py
+++ django_root/tutorial/settings/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'myproject',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '',
     }
 }
